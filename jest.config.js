2d7f54bca01cf90e5be28c121836df3f9df8aee4
william-jiang-lai <wjiang@indocresearch.org>
2022-05-06 14:36:48 -0400

    fix jest config for test cases

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,26 @@
 module.exports = {
   projects: [
-    // {
-    //   displayName: 'Serial Test Cases',
-    //   preset: 'jest-puppeteer',
-    //   runner: 'jest-serial-runner',
-    //   testMatch: [
-    //     '<rootDir>/tests/e2e/tests/**/?(*.)+(serial-test).js',
-    //     '<rootDir>/tests/critical-tests/tests/**/?(*.)+(critical.serial-test).js',
-    //   ],
-    // },
-    // {
-    //   displayName: 'Parallel Test Cases',
-    //   preset: 'jest-puppeteer',
-    //   testMatch: [
-    //     '<rootDir>/tests/e2e/tests/**/*.test.js',
-    //     '<rootDir>/tests/critical-tests/tests/**/*.critical.test.js',
-    //   ],
-    // },
+    {
+      displayName: 'Serial Test Cases',
+      preset: 'jest-puppeteer',
+      runner: 'jest-serial-runner',
+      testMatch: [
+        '<rootDir>/tests/e2e/tests/**/?(*.)+(serial-test).js',
+        '<rootDir>/tests/critical-tests/tests/**/?(*.)+(critical.serial-test).js',
+      ],
+    },
     {
       displayName: 'Parallel Test Cases',
       preset: 'jest-puppeteer',
-      testMatch: ['<rootDir>/tests/critical-tests/tests/sample.test.js'],
+      testMatch: [
+        '<rootDir>/tests/e2e/tests/**/*.test.js',
+        '<rootDir>/tests/critical-tests/tests/**/*.critical.test.js',
+      ],
     },
+    // {
+    //   displayName: 'Parallel Test Cases',
+    //   preset: 'jest-puppeteer',
+    //   testMatch: ['<rootDir>/tests/critical-tests/tests/sample.test.js'],
+    // },
   ],
 };
