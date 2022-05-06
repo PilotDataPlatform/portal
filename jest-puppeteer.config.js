827fd97c2ca86ea410b66471304252e83bc45a8f
Derek Luu <dluu@indocresearch.org>
2022-05-06 14:54:28 -0400

    add args to run puppeteer on ubuntu

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,6 +2,6 @@ module.exports = {
   launch: {
     headless: false,
     slowMo: 100,
-    args: [`--window-size=${1920},${1080}`],
+    args: [`--window-size=${1920},${1080}, --no-sandbox`],
   },
 };
