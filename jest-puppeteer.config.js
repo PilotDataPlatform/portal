ab253f134ad4c8c0da1ea1f5e35c9a5f8e7828e7
Derek Luu <dluu@indocresearch.org>
2022-05-09 12:36:51 -0400

    remove install chromium, set headless to true, remove executable path in puppeteer config

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,10 +1,11 @@
+const os = 'notLinux'
 let launch = {
   headless: false,
   slowMo: 100,
   args: [`--window-size=${1920},${1080}, --no-sandbox`],
 };
 launch = process.platform === 'linux'
-    ? { ...launch, executablePath: '/usr/bin/chromium-browser' }
+    ? { ...launch, headless: true }
     : launch;
 console.log(process.platform)
 
