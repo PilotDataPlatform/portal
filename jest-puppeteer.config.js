0902ae2e2602da7482eaca2abd560e7c7ada2bce
Derek Luu <dluu@indocresearch.org>
2022-05-09 12:07:33 -0400

    update puppeteer config, install chromium for ubuntu

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,13 @@
+let launch = {
+  headless: false,
+  slowMo: 100,
+  args: [`--window-size=${1920},${1080}, --no-sandbox`],
+};
+launch = process.platform === 'linux'
+    ? { ...launch, executablePath: '/usr/bin/chromium-browser' }
+    : launch;
+console.log(process.platform)
+
 module.exports = {
-  launch: {
-    headless: false,
-    slowMo: 100,
-    args: [`--window-size=${1920},${1080}, --no-sandbox`],
-  },
+  launch,
 };
