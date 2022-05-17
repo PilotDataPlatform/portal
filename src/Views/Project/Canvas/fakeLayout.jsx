e7dd49bed1dfece686b488f13d5b4e6dec5dc5a3
Derek Luu <dluu@indocresearch.org>
2022-05-17 15:14:01 -0400

    first pass at chart card meta data ui

--- src/Views/Project/Canvas/fakeLayout.jsx
+++ src/Views/Project/Canvas/fakeLayout.jsx
@@ -1,10 +1,18 @@
-
 const info = {
   type: 'info',
   key: '0',
   title: 'Project Information',
   content:
     'stands for GErman NEtwork for Research on AuToimmune Encephalitis. Our mission is to address the medical community working on autoimmuneencephalitis as well as all patients and their relatives touched by this condition.',
+  expandable: false,
+  exportable: false,
+};
+
+const charts = {
+  type: 'charts',
+  key: '1',
+  title: 'Charts',
+  defaultSize: 'm',
   expandable: false,
   exportable: false,
 };
@@ -38,18 +46,18 @@ const userStats = {
 };
 
 const superset = {
-  type:'superset',
-  key:'4',
-  title:"Superset",
+  type: 'superset',
+  key: '4',
+  title: 'Superset',
   defaultSize: 'm',
   expandable: false,
   exportable: false,
-}
+};
 
 const cardsAttr = {
   initial: [],
   //admin: [info, fileStats, files, userStats,superset],
-  admin: [fileStats, files, userStats],
+  admin: [fileStats, charts, userStats],
   contributor: [
     fileStats,
     {
