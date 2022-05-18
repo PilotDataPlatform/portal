32d00c78fb38d8c366058130fa5075bd3328e556
Derek Luu <dluu@indocresearch.org>
2022-05-18 10:32:49 -0400

    fix bug with option value

--- src/Views/Project/Canvas/Charts/FileExplorer/Plugins/VirtualFolders/VirtualFolderModal.jsx
+++ src/Views/Project/Canvas/Charts/FileExplorer/Plugins/VirtualFolders/VirtualFolderModal.jsx
@@ -65,6 +65,7 @@ const VirtualFolderModal = ({ visible, setVisible, files }) => {
     // eslint-disable-next-line
   }, [visible]);
 
+
   async function addToExistFolder(values) {
     try {
       const folderGeid = values.folder;
@@ -174,7 +175,7 @@ const VirtualFolderModal = ({ visible, setVisible, files }) => {
               <Select>
                 {vfolders.map((v) => {
                   return (
-                    <Option key={v.geid} value={v.geid}>
+                    <Option key={v.id} value={v.id}>
                       {v.name}
                     </Option>
                   );
