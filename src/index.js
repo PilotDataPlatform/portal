382a568e03f80470ba222001b26ebb02043d0c5a
william-jiang-lai <wjiang@indocresearch.org>
2022-06-07 11:44:59 -0400

    add process obj to index.html

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { store, persistor } from './Redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import KeyCloakMiddleware from './KeyCloakMiddleware';
 import './i18n';
-window.process = {};
 ReactDOM.render(
   <CookiesProvider>
     <Provider store={store}>
