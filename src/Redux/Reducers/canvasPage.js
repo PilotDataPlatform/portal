0c4bf29b2d90b411772717d56d54360916f9cba1
Derek Luu <dluu@indocresearch.org>
2022-06-17 15:38:05 -0400

    remove console.logs

--- src/Redux/Reducers/canvasPage.js
+++ src/Redux/Reducers/canvasPage.js
@@ -3,8 +3,6 @@ import { SET_CANVAS_PAGE } from '../actionTypes';
 const init = { page: 'greenroom-home' };
 function canvasPage(state = init, action) {
   const { type, payload } = action;
-  console.log('reducers');
-  console.log(payload);
   switch (type) {
     case SET_CANVAS_PAGE: {
       return payload;
