33b826f5d06263b43bf5032bbaada84999bde308
william-jiang-lai <wjiang@indocresearch.org>
2022-07-26 14:39:59 -0400

    add copy right to related source files

--- src/config.js
+++ src/config.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2022 Indoc Research
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 const BRANDING_PREFIX = process.env.REACT_APP_BRANDING_PATH || '';
 const PORTAL_PREFIX = process.env.REACT_APP_PORTAL_PATH || '';
 const DOWNLOAD_PREFIX_V2 = process.env.REACT_APP_DOWNLOAD_URL_V2 || '';
