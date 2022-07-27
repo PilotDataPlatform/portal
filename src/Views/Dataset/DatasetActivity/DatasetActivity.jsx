870d5fe7c8375c1064f4291db1be29fc61f09318
Eleanor Jiao <45496409+eleanoryy@users.noreply.github.com>
2022-07-27 09:39:11 -0400

    update DatasetActivity

--- src/Views/Dataset/DatasetActivity/DatasetActivity.jsx
+++ src/Views/Dataset/DatasetActivity/DatasetActivity.jsx
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2022 Indoc Research
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Table, DatePicker } from 'antd';
@@ -106,7 +90,7 @@ const DatasetActivity = (props) => {
     let queryParams;
     let toTime = moment().endOf('day').unix();
     let fromTime;
-    console.log(datasetInfo);
+
     const params = {
       page_size: pageSize,
       // page_size: 50,
